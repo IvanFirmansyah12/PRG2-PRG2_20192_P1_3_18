531d5830a4588b221a3730a2182b6bf59f64274c
Muh-Ivan-Firmansyah <muhivanfirmansyah@gmail.com>
2020-04-29 15:35:46 +0700

    initial commit

---
+++ PRG2_20192_P1_3_18/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace PRG2_20192_P1_3_18
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int angka;
+
+            Console.WriteLine(" Program Syair Anak Ayam ");
+            Console.Write(" Masukan Nilai N = ");
+            angka = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine(" ANAK AYAM TURUN {0} \n", angka);
+            for (int i = angka; i >= 1; i--)
+            {
+                if (i == 1)
+                {
+                    Console.WriteLine(" Anak Ayam Turun {0}, Mati satu tinggal induknya ", i);
+                }
+                else
+                {
+                    Console.WriteLine(" Anak Ayam Turun {0}, Mati satu tinggal {1} ", i, i - 1);
+                }
+            }
+        }
+    }
+}
